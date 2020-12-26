119b87af852aa338c812a1a8837f6487c0e9f817
Lucas Liaño <lucas.liano2000@gmail.com>
2020-12-26 14:53:50 -0300

    Images Update!

--- db/Prueba.sql
+++ db/Prueba.sql
@@ -43,14 +43,15 @@ DROP TABLE cat;
 DROP TABLE trago;
 
 /** Tragos Tropicales **/
-INSERT INTO trago VALUES (0, "Mojito", "Toda una Leyenda" ,"Ron, Menta, Tónica, Azúcar, Hielo.", true, 1);
-INSERT INTO trago VALUES (0, "Mojito Red", "Para Inovadores" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 1);
-INSERT INTO trago VALUES (0, "Margarita", "Un Clásico" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 1);
-INSERT INTO trago VALUES (0, "Margarita Blue", "Caribbean Style" ,"Ron, Menta, Tónica, Azúcar, Hielo.", true, 1);
-INSERT INTO trago VALUES (0, "Sex on the Beach", "Para Renovar la Pasión" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 1);
-INSERT INTO trago VALUES (0, "Agua de Valencia", "Sentite en España" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 1);
-INSERT INTO trago VALUES (0, "Caipirinha", "Para Sentirse en Brasil" ,"Ron, Menta, Tónica, Azúcar, Hielo.", true, 1);
-INSERT INTO trago VALUES (0, "Cubalibre", "Ron con Cola pai" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 1);
+INSERT INTO trago VALUES (0, "Mojito", "Toda una Leyenda" ,"Ron, Menta, Lima, Tónica, Azúcar.", true, 1);
+INSERT INTO trago VALUES (0, "Mojito Red", "Para Innovadores" ,"Ron, Menta, Frutilla, Lima, Tónica, Azúcar.", false, 1);
+INSERT INTO trago VALUES (0, "Margarita", "Un Clásico" ,"Tequila, Jugo de Limón, Azúcar.", false, 1);
+INSERT INTO trago VALUES (0, "Margarita Blue", "Caribbean Style" ,"Tequila, Blue Curacao, Jugo de Limón, Azúcar.", true, 1);
+INSERT INTO trago VALUES (0, "Sex on the Beach", "Para Renovar la Pasión" ,"Vodka, Licor de Durazno, Jugo de Naranja, Granadina.", false, 1);
+INSERT INTO trago VALUES (0, "Agua de Valencia", "Sentite en España" ,"Vodka, Ginebra, Champagne dulce, Jugo de Naranja, Jugo de Limón, Azúcar.", false, 1);
+INSERT INTO trago VALUES (0, "Caipirinha", "Para Sentirse en Brasil" ,"Cachaza, Lima, Tónica, Azúcar.", true, 1);
+INSERT INTO trago VALUES (0, "Cubalibre", "Para sentirte como el 'Che'" ,"Ron, Cola, Jugo de Limón, Angostura.", false, 1);
+INSERT INTO trago VALUES (0, "Gancia Batido con Limón", "Último pero no menos importante" ,"Gancia, Azúcar, Jugo de Limón, Rodajas de Limón.", false, 1);
 
 
 /** Bebidas sin alcohol **/
@@ -62,14 +63,14 @@ INSERT INTO trago VALUES (0, "Agua", "" ,"", false, 2);
 INSERT INTO trago VALUES (0, "Soda", "" ,"", false, 2);
 
 /** Tragos sin alcohol **/
-INSERT INTO trago VALUES (0, "San Francisco", "Inofensivo y Refrescante" ,"Ron, Menta, Tónica, Azúcar, Hielo.", true, 3);
-INSERT INTO trago VALUES (0, "Shirley Temple", "Para Bailar Sin Parar" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 3);
-INSERT INTO trago VALUES (0, "Soda Verde", "Sentite Ecológico" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 3);
+INSERT INTO trago VALUES (0, "San Francisco", "Inofensivo y Refrescante" ,"Jugo de Naranja, Jugo de Durazno, Jugo de Ananá, Granadina, Azúcar.", true, 3);
+INSERT INTO trago VALUES (0, "Shirley Temple", "Para Bailar Sin Parar" ,"Ginger Ale, Jugo de Naranja, Granadina.", false, 3);
+INSERT INTO trago VALUES (0, "Soda Verde", "Sentite Ecológico" ,"Jugo de Limón, Menta, Arándanos, Pepino, Soda.", false, 3);
 
 /** Tragos Secos **/
-INSERT INTO trago VALUES (0, "Gin-Tonic", "Para Calentar la Garganta" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 4);
+INSERT INTO trago VALUES (0, "Gin-Tonic", "Para Calentar la Garganta" ,"Gin, Tónica, Limón.", false, 4);
 INSERT INTO trago VALUES (0, "Martini", "Áspero como Lengua de Gato" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 4);
-INSERT INTO trago VALUES (0, "Aperol Spritz", "Para Sentirte a la Moda" ,"Ron, Menta, Tónica, Azúcar, Hielo.", true, 4);
+INSERT INTO trago VALUES (0, "Aperol Spritz", "Para Sentirte a la Moda" ,"Aperol, Champagne, Naranja.", true, 4);
 
 
 /** Cervezas **/
@@ -78,21 +79,22 @@ INSERT INTO trago VALUES (0, "Imperial APA", "American Pale Ale" ,"IBU:30 | ABV:
 INSERT INTO trago VALUES (0, "Imperial Lager", "Amber" ,"IBU:18 | ABV:5.5%", false, 5);
 
 /** Espumantes **/
-INSERT INTO trago VALUES (0, "Champagne Emilia", "Toda una leyenda" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 6);
-INSERT INTO trago VALUES (0, "Sidra 1888", "Toda una leyenda" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 6);
+INSERT INTO trago VALUES (0, "Champagne Emilia", "Burbujeante" ,"", false, 6);
+INSERT INTO trago VALUES (0, "Sidra 1888", "Una leyenda" ,"", false, 6);
 
 
 /** Vinos **/
-INSERT INTO trago VALUES (0, "Vinoide", "Toda una leyenda" ,"Ron, Menta, Tónica, Azúcar, Hielo.", false, 7);
+INSERT INTO trago VALUES (0, "Cosecha Tardia", "Fresco y Dulce" ,"", false, 7);
 
-
+DELETE FROM trago WHERE id = 16;
 
 SELECT * FROM trago;							/** This **/
 
 INSERT INTO pedido VALUES (0, "Mojito", "Juan", true );
 SELECT * FROM pedido;
 SELECT * FROM pedido WHERE terminado = false;
-DELETE FROM pedido WHERE idPedido = 1;
+
+
 drop table pedido;
 
 
